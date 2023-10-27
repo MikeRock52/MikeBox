68b4f78fc4aa5f26e71b378e5350dc14670ca7b0
MikeRock51 <mikerockmusic51@gmail.com>
2023-10-27 07:14:45 +0100

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import awsConfig from './aws-exports';
+import Navbar from './components/ui/nav/Navbar';
 
 Amplify.configure(awsConfig);
 
 function App({signOut, user}) {
   return (
     <div className="App text-lime-700">
+      <Navbar />
       <h1>Hello {user.username}</h1>
       <button onClick={signOut}>Sign out</button>
     </div>
