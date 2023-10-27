ae46aec0d83235af211bc6368e78de7e15bc724a
MikeRock51 <mikerockmusic51@gmail.com>
2023-10-27 17:29:49 +0100

    Implement updare user profile ToDO: Add profile update form

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Auth } from "aws-amplify";
+import { useFormik } from "formik";
 
-function Profile() {
+function Profile({ user }) {
+  const formik = useFormik({
+    initialValues: {
+      username: user.username,
+      email: user.attributes.email,
+    },
+    onSubmit: async (values) => {
+      try {
+        const result = await Auth.updateUserAttributes(user, {
+          username: formik.values.username,
+          email: formik.values.email,
+        });
+        console.log(result);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  });
   return (
-    <div>Profile</div>
-  )
+    <div>
+      Profile
+      <button onClick={formik.handleSubmit}>Submit</button>
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
