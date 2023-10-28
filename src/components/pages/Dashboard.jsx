f09d864c8bf897e3c6a3fb534566052a6f631a4d
MikeRock51 <mikerockmusic51@gmail.com>
2023-10-28 18:41:08 +0100

    Test Icon function Low battery

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { UploadIcon } from "../Icons";
 
 function Dashboard() {
   return (
     <div>
       <div className="">
         <button className="grid grid-col-1 align-middle rounded-md border-2 border-lime-700 mx-12 w-32 h-20 hover:bg-lime-300">
+          {UploadIcon()}
           <h4 className="w-fit h-fit my-auto ml-4 font-bold">Upload</h4>
         </button>
       </div>
