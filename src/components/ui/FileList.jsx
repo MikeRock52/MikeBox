5f4caf8ee611ae0633a7c464e3fcb7100ea30e99
MikeRock51 <mikerockmusic51@gmail.com>
2023-10-30 05:36:36 +0100

    Add collection component

--- src/components/ui/FileList.jsx
+++ src/components/ui/FileList.jsx
@@ -1,5 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Storage } from "aws-amplify";
+import {
+  Collection,
+  Card,
+  Heading,
+  Text,
+  Flex,
+  useTheme,
+  ThemeProvider,
+} from "@aws-amplify/ui-react";
+import theme from "./collectionTheme";
 
 function FileList({ upload }) {
   const [files, setFiles] = useState(null);
@@ -16,7 +26,69 @@ function FileList({ upload }) {
 
   files && console.log(files);
 
-  return <div>FileList</div>;
+  const { tokens } = useTheme();
+  const items = [
+    {
+      title: "Fiordland National Park",
+      description:
+        "This national park includes the famous fjords of Milford, Dusky and Doubtful Sounds.",
+    },
+    {
+      title: "Bay of Islands, North Island",
+      description:
+        "Three hours north of Auckland, this area features over 144 islands to explore.",
+    },
+    {
+      title: "Queenstown, South Island",
+      description:
+        "This hopping town is New Zealand's adventure capital and is located right on Lake Wakatipu.",
+    },
+    {
+      title: "Queenstown, South Island",
+      description:
+        "This hopping town is New Zealand's adventure capital and is located right on Lake Wakatipu.",
+    },
+  ];
+
+  return (
+    <div>
+      <ThemeProvider theme={theme} colorMode="dark">
+        <Collection
+          type="list"
+          items={items}
+          padding="2rem"
+          // maxWidth="1100px"
+          margin="0 auto"
+          justifyContent="center"
+          gap="small"
+          direction="row"
+          wrap="wrap"
+          isPaginated
+          isSearchable
+          searchNoResultsFound={
+            <Flex justifyContent="center">
+              <Text color="#b91c1c" fontSize="1rem">
+                No files found, please try again
+              </Text>
+            </Flex>
+          }
+          searchPlaceholder="Type to search file..."
+        >
+          {(item, index) => (
+            <Card
+              key={index}
+              padding={tokens.space.medium}
+              maxWidth="180px"
+              fontSize={tokens.fontSizes.xs}
+            >
+              <Heading level={4}>{item.title}</Heading>
+              <Text>{item.description}</Text>
+            </Card>
+          )}
+        </Collection>
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default FileList;
---
+++ src/components/ui/collectionTheme.js
@@ -0,0 +1,65 @@
+const theme = {
+  name: "collection-theme",
+  tokens: {
+    components: {
+      collection: {
+        pagination: {
+          current: {
+            color: { value: "#d9f99d" },
+            backgroundColor: {
+              value: "#4d7c0f",
+            },
+          },
+          button: {
+            color: { value: "{colors.blue.60}" },
+            _hover: {
+              backgroundColor: {
+                value: "{colors.blue.10}",
+              },
+              color: { value: "{colors.blue.60}" },
+            },
+          },
+        },
+        search: {
+          input: {
+            color: { value: "{colors.blue.60}" },
+          },
+          button: {
+            color: { value: "#4d7c0f" },
+            // backgroundColor: { value: "#4d7c0f"},
+            _focus: {
+              backgroundColor: {
+                value: "#4d7c0f",
+              },
+              color: {
+                value: "#d9f99d",
+              },
+            },
+            _hover: {
+              backgroundColor: {
+                value: "#4d7c0f",
+              },
+              color: {
+                value: "#d9f99d",
+              },
+            },
+          },
+        },
+      },
+      card: {
+        backgroundColor: { value: '{colors.background.secondary}' },
+        outlined: {
+          borderColor: { value: '{colors.black}' },
+        },
+      },
+      heading: {
+        color: { value: '{colors.brand.secondary[80]}' },
+      },
+      text: {
+        color: { value: '{colors.brand.primary[80]}' },
+      },
+    },
+  },
+};
+
+export default theme;
