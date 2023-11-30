d48d2caf748626d2e9bf4bb9041ad6353010c83d
MikeRock51 <mikerockmusic51@gmail.com>
2023-11-30 11:58:30 +0100

    Add App Title and description

--- src/components/ui/FileActions.jsx
+++ src/components/ui/FileActions.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { UploadIcon, CreateFolder, ToFolder, ViewFiles } from "../Icons";
+import { UploadIcon, CreateFolder, ViewFiles } from "../Icons";
 import { FileContexts } from "../../contexts/FileContexts";
 
 function FileActions() {
